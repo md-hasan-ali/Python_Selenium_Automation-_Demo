0f5e1f19181dc1c0457455b1e0c67524b5883cbc
md-hasan-ali <hasanaliringku@gmail.com>
2024-02-11 00:33:53 +0600

    Python Selenium Demo

---
+++ scheduler.py
@@ -0,0 +1,35 @@
+# selenium 4
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
+import time
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
+# Event Scheduler page 
+driver.get("Event Scheduler page link") 
+
+# Event title 
+title = driver.find_element(By.XPATH, "title element(locator)")
+title.send_keys("Title Name")
+
+#Event date
+date = driver.find_element(By.XPATH, "date(locator)")
+date.send_keys("Date")
+
+# Event participants 
+participants = driver.find_element(By.XPATH, "participants(locator)")
+participants.send_keys("participants")
+
+#Submit 
+submit = driver.find_element(By.XPATH, "submit(locator)")
+submit.click()
+
+#Verity that the event appears 
+event_list = driver.find_element(By.XPATH, "event_list(locator)")
+assert event_list
+
+
+time.sleep(3)
